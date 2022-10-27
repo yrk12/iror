bc53663827e24e55181243eb4cc3f5b86f2eb381
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-27 16:09:13 +0530

    added login validation

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./connect")
+const pool = require("./connect");
+const { response } = require("express");
 app.use(cors());
 app.use(express.json());
 
@@ -9,7 +10,6 @@ app.post("/register", async(req, res) =>{
     try {
         req=req.body;
         console.log(req);   
-        console.log(req.password);
         const newUser = await pool.query(
             "INSERT INTO Users (FirstName, LastName, Email, ContactNo, Password) VALUES ($1, $2, $3, $4, $5)",
             [req.fname, req.lname, req.email, req.contactNo, req.Password]
@@ -20,6 +20,30 @@ app.post("/register", async(req, res) =>{
     }
 });
 
+app.post("/login", async(req, res) =>{
+    try {
+        req=req.body;
+        console.log(req);   
+        let User = await pool.query(
+            "SELECT * FROM USERS WHERE EMAIL=$1",
+            [req.email]
+        );
+        User=User.rows;
+        if(User.length == 0){
+            res.json({success: false});
+        }
+        else{
+            if(User[0].password == req.Password){
+                res.json({success: true});
+            }
+            else{
+                res.json({success: false});
+            }
+        }
+    } catch (err) {
+        res.json({success: false});
+    }
+});
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
