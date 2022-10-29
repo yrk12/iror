0f7f42aaf6e0a27c1374a1bc356bb498db785912
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-29 23:07:37 +0530

    backend for search train by trainid and search train by station

--- frontend/src/files/components/trainsearch.js
+++ frontend/src/files/components/trainsearch.js
@@ -1,20 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "../files.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 function Trainsearch() {
+  const [ID, setID] = useState({
+    tID: ""
+  });
+
+  
+  function handleChange(event){
+    const newValue = event.target.value;
+    setID(prevValue => {return{
+       tID: newValue
+      }
+    })
+  }
+
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    console.log(ID);
+    try {
+      const response = await fetch("http://localhost:5000/getRoute", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(ID)
+        });
+        let res=await response.json();
+        console.log(res);
+    }
+    catch (err){
+      console.log(err);
+    }
+  }
+
   return (
     <div>
       <p>Search Train</p>
       <br />
       <form>
-        <TextField required id="outlined-required" label="Train Number" />
-        <TextField required id="outlined-required" label="Departure Date" />
+        <TextField required id="outlined-required" label="Train Number" onChange={handleChange}/>
         <br />
         <br />
-        <Button variant="contained">FIND</Button>
+        <Button onClick={ onSubmitForm } variant="contained">FIND</Button>
       </form>
     </div>
   );
