33576ec6dd6be24b274756e34f67534d9f4697e2
Deep Panchani <dlplearning00@gmail.com>
2022-10-29 23:14:52 +0530

    Login Page

--- frontend/src/files/login.js
+++ frontend/src/files/login.js
@@ -1,33 +1,31 @@
-import React , { useState } from "react";
-import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import trainImg from "./register_img.png";
 import "./files.css";
 
 function Login() {
-
   const [user, setUser] = useState({
     email: "",
-    Password: ""
+    Password: "",
   });
 
-  function handleChange(event){
+  function handleChange(event) {
     const newValue = event.target.value;
-    const inputname = event.target.name; 
-    setUser(prevValue => {
-      if(inputname === "email"){
+    const inputname = event.target.name;
+    setUser((prevValue) => {
+      if (inputname === "email") {
         return {
           email: newValue,
-          Password: prevValue.Password
+          Password: prevValue.Password,
         };
-      }
-      else{
+      } else {
         return {
           email: prevValue.email,
-          Password: newValue
+          Password: newValue,
         };
       }
-    })
+    });
   }
 
   const onSubmitForm = async (e) => {
@@ -36,49 +34,51 @@ function Login() {
       const response = await fetch("http://localhost:5000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(user)
+        body: JSON.stringify(user),
       });
-      let res=await response.json();
+      let res = await response.json();
       console.log(res);
-    }
-    catch (err){
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="login_page">
       <div className="login_main">
         <div className="train_image">
-          <img src={trainImg}/>
+          <img src={trainImg} />
         </div>
         <div className="login_input">
           <div>
             <h3>Login</h3>
             <br />
             <TextField
-              sx={{width: 319}}
+              sx={{ width: 319 }}
               required
               id="outlined-required"
               name="email"
               label="Email"
-              value={ user.email }
+              value={user.email}
               onChange={handleChange}
             />
-            <br /><br />
+            <br />
+            <br />
             <TextField
-              sx={{width: 319}}
+              sx={{ width: 319 }}
               id="outlined-password-input"
               required
               label="Password"
               name="password"
               type="password"
               autoComplete="current-password"
-              value={ user.Password }
+              value={user.Password}
               onChange={handleChange}
             />
             <br /> <br />
-            <Button onClick={ onSubmitForm } variant="contained">Login</Button>
+            <Button onClick={onSubmitForm} variant="contained">
+              Login
+            </Button>
           </div>
         </div>
       </div>
