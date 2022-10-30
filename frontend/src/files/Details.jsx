cf613071fffd629fe076162c9c4f4ad891c8c15c
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-30 23:34:14 +0530

    displaying train details

--- frontend/src/files/Details.jsx
+++ frontend/src/files/Details.jsx
@@ -1,59 +1,39 @@
 import React from "react";
 import "./Details.css";
 
-function Details() {
+function Details(props) {
+    props=props.props;
+
+    const months={"01": "Jan","02": "Feb","03": "Mar","04": "Apr","05": "May","06": "Jun","07": "Jul","08": "Aug","09": "Sep","10": "Oct","11": "Nov","12": "Dec"};
+
     return (
         <div className="trainDetails">
-            <h1 style={{margin: '30px 100px'}}>Book Your Ticket</h1>
-            <div className="booking">
-                <div className="book">
-                    <p>Departure</p>
-                    <input type="time" name="" id="" />
-                </div>
-                <div className="book">
-                    <p>Arival</p>
-                    <input type="time" name="" id="" />
-                </div>
-                <div className="book">
-                    <p>Date</p>
-                    <input type="date" name="" id="" />
-                </div>
-                <div className="book">
-                    <button style={{backgroundColor: '#1976D2', marginTop: '50px'}}>Modify Search</button>
-                </div>
-            </div>
-            <h3 style={{margin: '40px 100px '}}>3 Results</h3>
-            <div className="information">
-                <div className="data"><h3>Train Name and No</h3></div>
-                <div className="data"><h3>Departure</h3></div>
-                <div className="data"><h3>Duration</h3></div>
-                <div className="data"><h3>Arrival</h3></div>
-            </div>
+            
             <div>
-                <div className="information">
+                <div className="information"> 
                     <div className="box">
-                        <h3>NGP DURONTO</h3>
-                        <h3>12340</h3>
-                        <h4 style={{color: '#4CAF50'}}>30 Left</h4>
+                        <h3>{props.trainName}</h3>
+                        <h3>{props.trainid}</h3>
+                        <h4 style={{color: '#4CAF50'}}>{props.remainingSeats} Left</h4>
                         <div style={{display: 'flex'}}>
                             <button>Book Now</button>
-                            <h4 style={{color: '#03A9F4'}}>₹2,100</h4>
+                            <h4 style={{color: '#03A9F4'}}>₹{props.price}</h4>
                         </div>
                     </div>
                     <div className="box">
-                        <h3>Nagpur</h3>
-                        <h4>14:05</h4>
-                        <h4>Sat, 29 Oct</h4>
+                        <h3>{props.departure}</h3>
+                        <h4>{props.departureTime}</h4>
+                        <h4>{ props.departureDate.slice(8, 10) } {months[props.departureDate.slice(5, 7)]}</h4>
                     </div>
                     <div className="box">
-                        <h5>20 Hrs 11 Mins</h5>
+                        <h5>{props.durationHours} Hrs {props.durationMinutes} Mins</h5>
                         <h5>Runs On</h5>
-                        <h5>Saturday</h5>
+                        <h5>{props.runsOn}</h5>
                     </div>
                     <div className="box">
-                        <h3>Mumbai</h3>
-                        <h4>10:11</h4>
-                        <h4>Sun, 30 Oct</h4>
+                        <h3>{props.arrival}</h3>
+                        <h4>{props.arrivalTime}</h4>
+                        <h4>{ props.arrivalDate.slice(8, 10) } {months[props.arrivalDate.slice(5, 7)]}</h4>
                     </div>
                 </div>
             </div>
