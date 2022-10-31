22ea948e2ff7f601ee9955709008fa72ea2988be
Yuvrajsinh Kamalia <yuvrajsinhkamalia28@gmail.com>
2022-10-31 22:10:20 +0530

    updating branch

--- frontend/src/files/mybookings.js
+++ frontend/src/files/mybookings.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Tickets from "./Tickets.jsx";
 import "./mybookings.css";
 
 function Mybookings () {
+
+    const getTickets = () => {
+        let userId = sessionStorage.getItem("userID");
+
+        console.log(userId);
+    }
+
+    React.useEffect(() => {
+        getTickets();
+    }, []);
+
+
     return (
         <div className="mybookings">
             <div className="mybookings-child"><h1>Your Bookings</h1></div>
